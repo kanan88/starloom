fe7b30c27ca6cdd83ff4beddeaf96161083cfa7d
Kanan Farzaliyev <kanan.farzali@gmail.com>
2025-01-14 01:30:10 +0100

    integrate vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,22 @@ import Footer from "./components/Footer";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import Story from "./components/Story";
+import { Analytics } from "@vercel/analytics/react";
 
 const App = () => {
   return (
-    <main className="relative min-h-screen w-screen overflow-x-hidden">
-      <Navbar />
-      <Hero />
-      <About />
-      <Features />
-      <Story />
-      <Contact />
-      <Footer />
-    </main>
+    <>
+      <main className="relative min-h-screen w-screen overflow-x-hidden">
+        <Navbar />
+        <Hero />
+        <About />
+        <Features />
+        <Story />
+        <Contact />
+        <Footer />
+      </main>
+      <Analytics />
+    </>
   );
 };
 
